83a67daefc654fb5c69414a34fa742258252c6b4
jasonarrey02 <jasonarrey02@gmail.com>
2025-09-03 17:11:57 +0200

    Add files via upload
    Lagt til skatt i kr samt trinnskatt retursetning

--- O1.java
+++ O1.java
@@ -2,22 +2,40 @@ import java.util.Scanner;
 
 class O1 {
     public static void main(String[] args) {
+        double trinnskatt = 0;
+        int skatt = 0;
+
         Scanner myobj = new Scanner(System.in);
         System.out.println("Hva er din bruttoinntekt?");
         int lønn = myobj.nextInt();
 
         if (lønn < 217400) {
-            System.out.println("Du betaler ikke trinnskatt");
+            trinnskatt = 0;
+            skatt = (int) (lønn * trinnskatt / 100);
         } else if (lønn < 306050) {
-            System.out.println("Du betaler 1,7% trinnskatt");
+            trinnskatt = 1.7;
+            skatt = (int) (lønn * trinnskatt / 100);
         } else if (lønn < 697150) {
-            System.out.println("Du betaler 4,0% trinnskatt");
+            trinnskatt = 4.0;
+            skatt = (int) (lønn * trinnskatt / 100);
         } else if (lønn < 942400) {
-            System.out.println("Du betaler 13,7% trinnskatt");
+            trinnskatt = 13.7;   // (check correct %)
+            skatt = (int) (lønn * trinnskatt / 100);
         } else if (lønn < 1410750) {
-            System.out.println("Du betaler 16,7% trinnskatt");
-        } else  {
-            System.out.println("Du betaler 17,7% trinnskatt.");
+            trinnskatt = 16.7;   // (check correct %)
+            skatt = (int) (lønn * trinnskatt / 100);
+        } else {
+            trinnskatt = 17.7;
+            skatt = (int) (lønn * trinnskatt / 100);
+
         }
+
+        System.out.println("*******************************");
+        System.out.println("Din bruttoinntekt er: " + lønn +
+                "\nDin trinnskatt er: " + trinnskatt + "%" +
+                "\nDin årlige skatt er: " + skatt + "kr" );
+        System.out.println("*******************************");
+
+
     }
 }
